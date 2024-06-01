b9a5e3c64ef3536e237ee8620afddfcec5894889
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2024-06-01 14:41:44 +0300

    Update Header component to improve layout and navigation

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,26 +1,23 @@
 'use client';
-import useAuth from '@/hooks/useAuth';
-import Link from 'next/link';
 import React from 'react'
 import AuthComponent from './AuthComponent';
+import { useRouter } from 'next/navigation';
+import ROUTES from '@/constants/routes';
 
 export default function Header() {
-  const { user, isLoggedIn, logout } = useAuth();
+  const router = useRouter();
+
   return (
-    <header className='
-    w-full
-    flex
-    items-center
-    justify-between
-    bg-gray-800
-    text-white
-    px-5
-    py-3
-    fixed
-    '>
-      <Link href='/' className='text-2xl font-bold'>
-        Simple Task App
-      </Link>
+    <header className='w-full flex items-center justify-between bg-zinc-900 shadow-md px-5 py-3 fixed'
+      style={{height: '10vh', zIndex: 1000}}
+    >
+      <h1 className='text-2xl font-bold text-cyan-200 cursor-pointer' style={{
+        textShadow: ` 0px 0px 10px rgb(34, 211, 238),
+                      0px 0px 15px rgb(34, 211, 238)`
+      }}
+        onClick={() => router.push(ROUTES.LANDING)}>
+        Simple Task
+      </h1>
       <AuthComponent />
     </header>
   )
