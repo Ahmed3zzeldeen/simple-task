4cd532ead8c631f998f0ebaa34b62484bec14951
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2024-06-01 14:49:31 +0300

    Refactor authentication pages to remove duplicate code and improve code structure

--- app/auth/forgot-password/page.tsx
+++ app/auth/forgot-password/page.tsx
@@ -2,15 +2,6 @@ import ResetPasswordForm from "@/components/ResetPasswordForm";
 
 export default function Page() {
   return (
-    <main className="
-      flex
-      h-screen
-      flex-col
-      items-center
-      justify-between
-      p-24
-    ">
-      <ResetPasswordForm />
-    </main>
+    <ResetPasswordForm />
   )
 }
