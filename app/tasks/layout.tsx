3fb991c2efe6810b0c6f425da7164189492500c0
Ahmed M. Ezzeldeen <Ahmed.Mohamed.Ezzeldeen@Gmail.com>
2024-06-06 15:49:17 +0300

    Update layout styles for better responsiveness and update text sizes

--- app/tasks/layout.tsx
+++ app/tasks/layout.tsx
@@ -29,7 +29,14 @@ export default function Layout({ children }: Readonly<{ children: React.ReactNod
 
   return (
     <div className='container flex justify-between items-start gap-6 flex-wrap md:flex-nowrap'>
-      <div className='flex flex-col gap-10 w-full lg:w-1/5 md:w-5/5 max-h-96 bg-zinc-800 rounded-md p-4'>
+      <div className='
+      flex 
+      flex-col 
+      gap-5 
+      w-full 
+      md:w-80
+      max-h-96 
+      bg-zinc-800 rounded-md p-4'>
         <h2 className='text-md font-bold text-purple-700'>Filters</h2>
         <TodoListNav />
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -8,13 +8,11 @@ export default function Header() {
   const router = useRouter();
 
   return (
-    <header className='w-full flex items-center justify-between bg-zinc-900 shadow-md px-5 py-3 fixed'
-      style={{height: '10vh', zIndex: 1000}}
-    >
-      <h1 className='text-2xl font-bold text-cyan-200 cursor-pointer' style={{
-        textShadow: ` 0px 0px 10px rgb(34, 211, 238),
-                      0px 0px 15px rgb(34, 211, 238)`
-      }}
+    <header className='w-full flex items-center justify-between bg-zinc-900 shadow-md px-5 py-3 fixed' style={{ height: '10vh', zIndex: 1000 }}>
+      <h1 className='text-lg sm:text-xl md:text-2xl lg:text-2xl xl:text-3xl font-bold text-cyan-200 cursor-pointer hover:text-cyan-100 transition-all duration-300 ease-in-out'
+        style={{
+          textShadow: `0px 0px 10px rgb(34, 211, 238),0px 0px 15px rgb(34, 211, 238)`
+        }}
         onClick={() => router.push(ROUTES.LANDING)}>
         Simple Task
       </h1>
